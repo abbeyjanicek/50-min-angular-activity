126d28068535098304d79c2e6a207a2effc11680
Abbey Janicek <abbey.janicek@gmail.com>
2018-08-08 14:00:53 -0500

    finished step 4

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,11 +3,11 @@ let foodApp = angular.module('foodApp', []);
 foodApp.controller('FoodController', function() {
     console.log('NG');
     let vm = this;
-    vm.foodList = [];
+    //vm.foodList = [];
 
     vm.addFood = function() {
         console.log('in addFood', vm.foodIn);
-        vm.foodList.push(vm.foodIn);
-        console.log('food list: ', vm.foodList);
+        //vm.foodList.push(vm.foodIn);
+        //console.log('food list: ', vm.foodList);
     }
 })
